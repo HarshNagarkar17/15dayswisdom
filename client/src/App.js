bb6fb6fd957dccd87b86fc0e17408670618746e6
Harsh Nagarkar <harshvn187@gmail.com>
2023-12-25 12:25:08 +0530

    coming soon message

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <section>
+    <div className="container">
+      <div className="holder d-flex align-items-center justify-content-center">
+        <h1  style={{ width: 'fit-content' }}>
+        🚀 We're busy crafting something awesome behind the scenes. Stay tuned for our big reveal – your next online adventure is just around the corner! 🌟 #ComingSoon
+        </h1>
+      </div>
+    </div>
+    </section>
+  );
+}
+
+export default App;
